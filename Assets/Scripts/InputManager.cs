cf34d07e8e21d6672021e25d1ecf17cceda64128
wivi514 <wivi514@hotmail.com>
2024-01-19 12:39:21 -0500

    FPS camera and movement
    Created Savage Reverie unity project added package Input System and Cinemachine. Added a first person camera (as a Cinemachine Virtual Camera) and implemented basic movement using Unity Input System

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls controls;
+    PlayerMovement pm;
+
+    public static Vector2 movementInput;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        pm = GameObject.Find("Player").GetComponent<PlayerMovement>();
+
+        // Enable the New Input System from unity
+        controls.Player.Enable();
+
+        // Will need to be placed somewhere else later in development
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Movement.performed += MoveInput;
+        controls.Player.Movement.canceled += StopMove;
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Movement.performed -= MoveInput;
+        controls.Player.Movement.canceled -= StopMove;
+    }
+
+    private void MoveInput(InputAction.CallbackContext ctx)
+    {
+        // Grab the input and set it as a vector2 variable
+        movementInput = ctx.ReadValue<Vector2>();
+        Debug.Log(movementInput);
+    }
+
+    private void StopMove(InputAction.CallbackContext ctx)
+    {
+        // When movement is canceled, set the movement input to zero to stop the player
+        movementInput = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 10f;
+    private float jumpForce = 1;
+    public bool isGrounded = false;
+    private byte raycastJump = 1;
+    private float speedJumpModifier = 0.5f; // Speed multiplier when player jumps
+
+    private Rigidbody rb;
+    private Camera playerCamera;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Do a raycast down to see if the player is on the ground
+        if (Physics.Raycast(transform.position, Vector3.down, raycastJump))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        Move(InputManager.movementInput); // Call the movement function in FixedUpdate
+    }
+
+    public void Move(Vector2 moveInput)
+    {
+        // Calculate the move direction based on camera orientation
+        Vector3 cameraForward = playerCamera.transform.forward;
+        Vector3 cameraRight = playerCamera.transform.right;
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+        Vector3 moveDirection = cameraForward.normalized * moveInput.y + cameraRight.normalized * moveInput.x;
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector3(moveDirection.x * speed, rb.velocity.y, moveDirection.z * speed);
+        }
+        else
+        {
+            rb.velocity = new Vector3(moveDirection.x * speed * speedJumpModifier, rb.velocity.y, moveDirection.z * speed * speedJumpModifier);
+        }
+    }
+}
