ac38f2e3f67ccb4a8223bda87ef2b55ece00b7c2
wivi514 <wivi514@hotmail.com>
2024-01-26 17:12:56 -0500

    Added interact and interactable code
    added a basic interact script that will say on the screen with what the player is interacting and interactable script for a door

--- Assets/Scripts/Interaction System/Interact.cs
+++ Assets/Scripts/Interaction System/Interact.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Interact : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Variables
+    public float interactionRange = 3f; // La distance ?laquel le joueur peut intéragir avec un objet
+    private TMP_Text interactionText; // Le texte ?l'écran permettant d'afficher si 
+
+    void Awake()
     {
-        
+        // attribue ?la variable interactionText le Text de l'UI pour l'interaction avec des objets.
+        interactionText = GameObject.Find("Interaction Text").GetComponent<TMP_Text>();
+        //Cache le texte d'interaction lorsque la partie commence 
+        interactionText.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Utilise la fonction UpdateUI() pour permettre d'afficher ?l'écran le UI d'interaction si il y ?un objets avec lequel ont peut intéragir
+        UpdateUI();
+    }
+
+    //Fonction qui permet d'intéragir avec les objets qui ont le script InteractableObject attach?sur eux
+    public void TryInteract()
+    {
+        // Envoie un raycast au centre de l'écran pour voir si il y ?un objet
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        // Regarde si il y a un objet avec lequel interagir ?la distance appropri?
+        if (Physics.Raycast(ray, out hit, interactionRange))
+        {
+            // Assigne le script InteractableObject de l'objet touch??la variable interactable object
+            Interactable interactableObject = hit.collider.GetComponent<Interactable>();
+
+            if (interactableObject != null) // Si l'objet touch??un Component InteractableObject
+            {
+                // Appel la méthode Interact du script InteractableObject de l'objet
+                interactableObject.Interact();
+            }
+        }
+    }
+
+    private void UpdateUI()
+    {
+        // Envoie un raycast au centre de l'écran pour voir si il y ?un objet
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+
+        // Regarde si il y a un objet avec lequel interagir ?la distance appropri?
+        if (Physics.Raycast(ray, out hit, interactionRange))
+        {
+            // Assigne le script InteractableObject de l'objet touch??la variable interactable object
+            Interactable interactableObject = hit.collider.GetComponent<Interactable>();
+
+            // Affiche le texte UI si il y ?un objet détect?par le raycast qui ?un composant InteractableObject
+            if (interactableObject != null)
+            {
+                interactionText.text = "Press 'E' to interact with " + interactableObject.gameObject.name; //Affiche le texte d'intéraction avec le nom de l'objet
+                interactionText.enabled = true;
+            }
+            else
+            {
+                // Si le raycast touche un objet qui ne peut pas être interragit avec ça enleve le texte d'interaction
+                interactionText.enabled = false;
+            }
+        }
+        else
+        {
+            // Si le raycast ne touche rien n'affiche pas le texte
+            interactionText.enabled = false;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        // Affiche le raycast envoyé par la caméra en tant que rayon de couleur cyan dans la fenêtre scene sur Unity si on a enable les Gizmos
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(ray.origin, ray.origin + ray.direction * interactionRange);
     }
 }
--- Assets/Scripts/Interaction System/Interactable.cs
+++ Assets/Scripts/Interaction System/Interactable.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Variables
+    [SerializeField] GameObject interactable;
 
-    // Update is called once per frame
-    void Update()
+    public void Interact()
     {
-        
+        if (interactable != null)
+        {
+            // Affiche l'objet avec lequel on intéragit dans la console Unity
+            Debug.Log("Interacting with " + gameObject.name);
+
+            //Regarde si l'objet à un tag "Button"
+            if (this.gameObject.CompareTag("Button"))
+            {
+                //Regarde si l'objet à un Animator
+                Animator doorAnimator = interactable.GetComponent<Animator>();
+
+                //Si l'objet à un animator
+                if (doorAnimator != null)
+                {
+                    // Regarde si la porte est ouverte
+                    bool isDoorOpen = doorAnimator.GetBool("doorOpen");
+
+                    AudioSource doorAudio = interactable.GetComponent<AudioSource>();
+                    // Ferme ou ouvre la porte dépendamment de si elle est déja ouverte en inversant le boolean
+                    doorAnimator.SetBool("doorOpen", !isDoorOpen);
+                    doorAudio.Play();
+                }
+            }
+        }
     }
 }
