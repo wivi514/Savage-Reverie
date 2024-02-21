8f7ed2e9bc21ee84ee6d3e1b8503046a0d902d87
wivi514 <wivi514@hotmail.com>
2024-02-20 21:18:55 -0500

    Pickable object and inventory
    Added comment and artwork that will be shown in inventory UI in the pickableObject script I also updated the name in the inventory UI to properly reflect the button usage

--- Assets/Scripts/Scriptable Object/PickableObject.cs
+++ Assets/Scripts/Scriptable Object/PickableObject.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Object Type", menuName = "Objects")]
 public class PickableObject : ScriptableObject
 {
-    public string objectName;
-    public string description;
-    public ObjectType type;
+    public string objectName; //Insert object name
+    public string description; //Insert object description
 
-    public int weight;
-    public int value;
+    public ObjectType type; //Select the type of the object
+    public Sprite uiArtwork; //Insert the sprite that will be shown in the UI it needs to be square
+
+    public int weight; //Insert object weight that'll be used to slow down the player if he has too much weight on him
+    public int value; //Insert monetary value of the object
 }
 
+//Here are all type of object that we'll sort in multiple menu depending of the type it is. Add more type if required(will also need to update the inventory UI accordingly)
 public enum ObjectType
 {
     Misc,
