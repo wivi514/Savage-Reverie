de6f175a49354b6d579e54f55d9bf10b14683ef9
wivi514 <wivi514@hotmail.com>
2024-04-19 14:57:27 -0400

    Added thing in scene Lemonde

--- Assets/Scripts/AI/AiNavigationScript.cs
+++ Assets/Scripts/AI/AiNavigationScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Interactable))]
+[RequireComponent(typeof(Inventory))]
+[RequireComponent(typeof(CharacterHealth))]
 
 public class AiNavigationScript : MonoBehaviour
 {
--- Assets/Scripts/Objects/SceneObjectInformation.cs
+++ Assets/Scripts/Objects/SceneObjectInformation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Interactable))]
 public class SceneObjectInformation : MonoBehaviour
 {
     public PickableObject scriptableObject;
